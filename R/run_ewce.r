1bc727d3f18702917c90b4a1a9d89c52bc7ee46d
nskene <nathan.skene@gmail.com>
2019-03-20 18:47:48 +0000

    Prepared with drake

---
+++ R/run_ewce.r
@@ -0,0 +1,12 @@
+run_ewce <- function(ctd,tt,thresh = 250){
+  ewce_spinal = ewce_expression_data(ctd,annotLevel=1,tt=tt,sortBy="t",thresh=thresh,reps=10000,ttSpecies = "human",sctSpecies="mouse")
+  ewce_spinal$joint_results$Q = p.adjust(ewce_spinal$joint_results$p)
+  ewce_spinal$joint_results[order(ewce_spinal$joint_results$sd_from_mean),]
+  
+  write.csv(ewce_spinal$joint_results,file="Results/Tables/EWCE_SPINAL.csv")
+  pdf(file="Results/Figures/Fig_EWCE_HumanSpinal.pdf",width=6,height=5)
+  print(ewce.plot(ewce_spinal$joint_results))
+  dev.off()
+  
+  return(ewce_spinal)
+}
---
+++ drake_make.R
@@ -0,0 +1,25 @@
+#############################################################
+## GSE18920 --- Splicing array data from human spinal cord ##
+#############################################################
+library(limma)
+library(One2One)
+library(tidyr)
+library(EWCE)
+library(R.utils)
+library(drake)
+
+load(file="Data/Tidy/ctd_OligosNCortex_(woLev)_thresh(0)_trim(0).rda")
+
+plan <- drake_plan(
+  path = "/Users/natske/Datasets that are too large to store elsewhere/Disease Transcriptomes/ALS/",
+  data_spinal = load_als_data(path),
+  annot_spinal = load_als_annot(path,data_spinal),
+  tt_spinal = run_als_diffExp_analysis(annot_spinal,data_spinal),
+  ewce_spinal = run_ewce(ctd,tt_spinal),
+  catchOut = generate.bootstrap.plots.for.transcriptome(sct_data=ctd,tt=tt_spinal,thresh=100,annotLevel=1,reps=1000,full_results=ewce_spinal,listFileName="Human ALS Spinal")
+)
+
+config <- drake_config(plan)
+vis_drake_graph(config)
+
+make(plan)
